cf4f60345b9857ed9281bc0b39091fa04191e28d
Akssasori <diniz3003@gmail.com>
2023-08-09 11:02:18 -0300

    88 adicionando dados com req via post

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
 
   const [products, setProducts] = useState([]); 
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   // 1 - resgatando dados
   useEffect(() => {
 
@@ -24,8 +27,24 @@ function App() {
    
   },[]);
 
-  console.log(products);
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  }
+  
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
@@ -36,6 +55,19 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className='add-product'>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              Price:
+              <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+            </label>
+            <input type="submit" value="criar" />
+          </form>
+      </div>
     </div>
   );
 }
